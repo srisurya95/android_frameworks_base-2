PureNexusProject-Legacy/android_frameworks_base

Statusbar clock and date customizations [1/2] Squashed commit of the following:
Author: Alex Cruz <mazdarider23@gmail.com>
Date:   Mon Apr 25 03:26:25 2016 -0400
When font size changes so should the left and center clock
- I wasn't sure if I was going crazy or what but finally looked into this and turns out that we
were only updating the right clock and in turn sometimes had a bigger or small left/center clock

Author: bigrushdog <randall.rushing@gmail.com>
Date:   Tue Dec 15 14:06:14 2015 -0500
statusbar: fix notification icons bleeding into center clock view
Pass StatusbarIconController to Clock instances. mIconController
was declared in Clock, but was never set.

Author: dwitherell <devonwitherell1@gmail.com>
Date:   Wed Nov 4 12:29:01 2015 -0500
Clock: fix left and center clock not using dark mode

Author: snak3ater <saeedyasir91@gmail.com>
Date:   Sat Oct 24 15:49:54 2015 +0500
SystemUI: Fix dual clock bug
- Center clock and right clock both are visible after clean flashing of rom.
- Also properly define Leftclock

Author: Jean-Pierre Rasquin <yank555.lu@gmail.com>
Date:   Sun Aug 16 15:13:00 2015 +0200
[2/2] Frameworks: Add option for left clock style / position date left-right of clock
This adds :
- clock positioned on the left
- option to position date left or right of clock

Author: camcory <camcory1@gmail.com>
Date:   Fri Oct 23 13:47:01 2015 -0400
Fix up clock and date customizations
Move to StatusBarIconController
Fix animate show/hide so the center clock disappears correctly

Author: Allan Hedelain <tristan202@gmail.com>
Date:   Sun Nov 23 19:57:49 2014 +0100
Frameworks: Statusbar clock and date customizations
Forward ported to marshmallow by liquid0624
Some things changed on kitkat + our old code needed in general a bit
rework. All done with this patchset.
Originally by kufikugel. I only ported it to 5.0

Thanks to @camcory for the overlapping center clock fix on 7.0

PureNexus Edits:
remove clock coloring
make aosp seconds option work with custom cloc

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mNetworkTraffic;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock, mNetworkTraffic;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -48,6 +48,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         mBattery = mView.findViewById(R.id.battery);
         mClock = mView.findViewById(R.id.clock);
+        mCenterClock = mView.findViewById(R.id.center_clock);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
@@ -94,7 +95,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mSignalCluster, newAlpha),
                     animateTransitionTo(mNetworkTraffic, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC)
+                    animateTransitionTo(mClock, newAlphaBC),
+                    animateTransitionTo(mCenterClock, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -108,6 +110,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mNetworkTraffic.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
+            mCenterClock.setAlpha(newAlphaBC);
         }
     }
 }

srisurya95/android_frameworks_base-2

Change-Id: Icb70608a0e67d54214f999587139be92214b185b

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mClock, mNetworkTraffic, mLogoIcon;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mClock, mNetworkTraffic, mLogoIcon, mCenterClock;
+
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -50,6 +51,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mClock = mView.findViewById(R.id.clock);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         mLogoIcon = mView.findViewById(R.id.status_bar_logo);
+        mCenterClock = mView.findViewById(R.id.center_clock);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -97,6 +99,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     //animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mLogoIcon, newAlpha)
+                    animateTransitionTo(mCenterClock, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -111,6 +114,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
 //            mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
             mLogoIcon.setAlpha(newAlpha);
+            mCenterClock.setAlpha(newAlphaBC);
         }
     }
 }